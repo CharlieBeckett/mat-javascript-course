bb1570945e985db43b15bc8a1a1b3dd95b572cab
CharlieBeckett <mrcharlieb@gmail.com>
2019-03-04 16:13:20 +0000

    Added protractor

---
+++ test/protractor.conf.js
@@ -0,0 +1,32 @@
+exports.config = {
+
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    baseUrl:'http://localhost:8080/',
+
+    framework: 'custom',
+    frameworkPath: require.resolve('protractor-cucumber-framework') ,
+
+
+    plugins: [{
+        package: 'protractor-multiple-cucumber-html-reporter-plugin',
+        options:{
+automaticallyGenerateReport: true,
+removeExistingJsonReportFile: true
+
+        }
+    }],
+
+    specs: [
+        'features/*.feature'
+    ],
+    cucumberOpts: {
+        require: 'feature/step_definitions/*.steps.js',
+        tags:false,
+        format:'json:.tmp/results.json',
+        profile:false,
+        'no-source':true
+
+
+    }
+
+}
